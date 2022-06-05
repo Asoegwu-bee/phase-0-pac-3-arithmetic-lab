aef3314acf840ca312da5a492855c1e35f116252
Bianca Emmanuel <bianca.emmanuel@student.moringaschool.com>
2022-06-05 12:39:20 +0100

    commit tree af9dcca997f9331d3945c3e22a75f228920cf165

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let number = a
+number += 
+number;
+number -=
+number;
+
+number = 5
+function add5() {
+    return (number += 2);
+}
+function subtract3() {
+    return (number -= 2);
+}
+
+makeint("a", 10);
+preserveDecimal("a", 10);
+preserveDecimal("2.222", 10);
